db20a0d433c80e86188985b484dec66c8873f953
russe <danil.2001.danil.2002@gmail.com>
2021-12-02 11:54:00 +0300

    5

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,9 @@
 # TODO здесь писать код
+number = int(input('Введите число: '))
+
+for n in range(2, number + 1):
+    if number % n != 0:
+        continue
+    else:
+        break
+print('Наименьший делитель, отличный от единицы: ', n)
