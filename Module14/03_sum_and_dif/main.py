6b74afd74c387c54a50d79db2242e496748a7182
russe <danil.2001.danil.2002@gmail.com>
2021-12-02 11:49:50 +0300

    3

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+number = int(input('Введите число: '))
+
+def summ(x):
+    s = 0
+    while x > 0:
+        s += x % 10
+        x //= 10
+    return s
+
+def amount(x):
+    s = 0
+    while x > 0:
+        x //= 10
+        s += 1
+    return s
+
+print('Сумма цифр: ', summ(number))
+
+print('Кол-во цифр в числе: ', amount(number))
+
+print('Разность суммы и кол-ва цифр: ', summ(number) - amount(number))
