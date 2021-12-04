d9b2940dd8105fa663e7ea2d8f9546170c9912fe
Serge K <ks7100313@gmail.com>
2021-12-04 13:57:12 +0000

    Ok with comments

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -3,6 +3,7 @@ coordinate_x = float(input('X: '))
 coordinate_y = float(input('Y: '))
 radius_maximum = float(input('Введите радиус: '))
 
+# hypotenuse =))
 radius_calculated = coordinate_x ** 2 + coordinate_y ** 2
 
 if radius_calculated > radius_maximum:
