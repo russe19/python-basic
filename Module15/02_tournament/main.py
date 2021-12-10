f793ad7d5cbca8d90805247721a077992f5fe8cf
Alexa <aguseva@mail.com>
2021-12-10 13:20:14 +0100

    решения нужно доработать

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -3,3 +3,7 @@ names_first_day = []
 for index in range(1, 8, 2):
     names_first_day.append(names[index])
 print(names_first_day)
+
+# TODO чётные индексы определены неверно
+# TODO правильный ответ должен получиться такой:
+#  ['Артемий', 'Влад', 'Дима', 'Женя']
