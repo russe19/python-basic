933d88ef6cc18c2252a6733726bf13f7998d9619
danildis1 <danil.2001.danil.2002@gmail.com>
2022-04-26 20:33:09 +0300

    1

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import os
+
+data = open('zen.txt', 'r', encoding='utf-8')
+open_data = data.read()
+data_st = open_data.split('\n')
+for i_elem in data_st[::-1]:
+    print(i_elem)
+data.close()
