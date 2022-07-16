0287f18ff6491c4bbd6d819432b9242328356644
Alexandra <aguseva@mail.com>
2022-07-16 12:34:16 +0200

    решение нужно доработать

--- Module31/05_pin_code/main.py
+++ Module31/05_pin_code/main.py
@@ -2,5 +2,9 @@ import itertools
 
 colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-for i in itertools.combinations_with_replacement(colors, 4):
-    print(i)
+i = 0
+for _ in itertools.combinations_with_replacement(colors, 4):
+    i += 1
+print(i)  # 715
+# TODO Всего вариантов должно быть 10 000 (математика: 10*10*10*10)
+#  сейчас выдаёт не всё, которые ожидаем потому, что здесь нужен другой метод или подход
